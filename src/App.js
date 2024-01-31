d0ac3e37f8904f8c7f58c818438ed6f451371d96
Yana94Ko <91898700+Yana94Ko@users.noreply.github.com>
2024-02-01 08:37:08 +0900

    [Feat] ProfileContext기능 추가

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import MoviesDetail from "./pages/MoviesDetailPage";
 import { AuthProvider } from "./contexts/auth.context";
 import MyPage from "./pages/MyPage";
+import { ProfileProvider } from "./contexts/profile.context";
 
 function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/sign-in" element={<SignInPage />} />\
-          <Route path="/my-page" element={<MyPage />} />
-          <Route path="/movies/:movieId" element={<MoviesDetail />} />
-        </Route>
-      </Routes>
+      <ProfileProvider>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/sign-in" element={<SignInPage />} />\
+            <Route path="/my-page" element={<MyPage />} />
+            <Route path="/movies/:movieId" element={<MoviesDetail />} />
+          </Route>
+        </Routes>
+      </ProfileProvider>
     </AuthProvider>
   );
 }
--- src/contexts/profile.context.js
+++ src/contexts/profile.context.js
@@ -1,17 +1,44 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
+import utils from "../utils/utils";
 
 const initialValue = {
   nickname: null,
+  profileImg: null,
   likedMovies: [],
 };
 
 const ProfileContext = createContext(initialValue);
 
 export function ProfileProvider({ children }) {
-  const [nickname, setNickname] = useState("");
+  const [nickname, setNickname] = useState(utils.nickname.getRandomNickName);
   const updateNickname = (newNickname) => setNickname(newNickname);
+  const [likedMovies, setLikedMovies] = useState([]);
+  const addLikedMovies = (movie) => {
+    setLikedMovies([...likedMovies, movie]);
+  };
+  const removeLikedMovies = (targetMovie) => {
+    setLikedMovies(likedMovies.filter((movie) => movie !== targetMovie));
+  };
+  const isMovieLiked = (movieId) => {
+    return likedMovies.includes((likedMovie) => likedMovie.id === movieId);
+  };
+  const likedMoviesUtil = {
+    addLikedMovies,
+    removeLikedMovies,
+    isMovieLiked,
+  };
+
   const value = {
     nickname,
     updateNickname,
+    likedMovies,
+    likedMoviesUtil,
   };
+  return (
+    <ProfileContext.Provider value={value}>{children}</ProfileContext.Provider>
+  );
 }
+
+export const useProfile = () => useContext(ProfileContext);
+
+export default ProfileContext;
